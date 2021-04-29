dc2e7a27f2dc1898ca339a4375f54477ac3ce930
vivek <vivek@athenaeum.in>
2021-04-29 07:43:46 +0530

    done

--- index.mjs
+++ index.mjs
@@ -3,19 +3,34 @@ import { produce } from 'immer';
 const originalState = {
   isLoggedIn: true,
   imageUrl: 'http://localhost:3000/image.png',
-  displayName: 'Nagesh',
+  user: {
+    firstName: 'Nagesh',
+    lastName: 'A',
+    extraInfo: {
+      roles: ['user', 'manager'],
+      office: {
+        location: 'Gurugram',
+      },
+    },
+  },
 };
 
 const nextState = produce(originalState, (d) => {
-  //  apply the change as mutation
-  //  but immer will cause immutable update
-  //  originalState is unchanged
-  d.displayName = 'Ravindra';
+  d.user.extraInfo.roles.shift(); //  delete manager
+  d.user.extraInfo.office.location = 'Chennai';
 });
 
 //  original state is unchanged
-console.log(originalState);
+console.log(originalState.user.extraInfo.roles);
+console.log(originalState.user.extraInfo.office.location);
 
-console.log(nextState);
+console.log(nextState.user.extraInfo.roles);
+console.log(nextState.user.extraInfo.office.location);
 
 console.log(originalState === nextState);
+
+//  it is not a shallow copy!
+console.log(originalState.user === nextState.user);
+console.log(originalState.user.extraInfo === nextState.user.extraInfo);
+
+//  more info: https://immerjs.github.io/immer/

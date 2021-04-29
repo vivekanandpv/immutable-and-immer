fe3da3c177d286368a00e06eccd5250a5b74e106
vivek <vivek@athenaeum.in>
2021-04-29 10:44:16 +0530

    done

--- index.mjs
+++ index.mjs
@@ -1,11 +1,33 @@
 import { produce } from 'immer';
 
-let state = {
-  firstName: 'Vivekanand',
-  city: 'Haveri',
-};
+//  store:
+//  1. encapsulates the state
+//  2. encapsulates the reducer
+//  3. provides the subscription for the consumers
 
-function reducer(originalState, action) {
+function Store(nascentState, originalReducer) {
+  let state = nascentState;
+  let reducer = originalReducer;
+
+  const invocationList = [];
+
+  this.subscribe = function (cb) {
+    invocationList.push(cb);
+  };
+
+  this.getState = function () {
+    return produce(state, (d) => {});
+  };
+
+  this.dispatch = function (action) {
+    state = reducer(state, action);
+    invocationList.forEach((cb) => {
+      cb();
+    });
+  };
+}
+
+const reducer = (originalState, action) => {
   //  apply the modifications to the newState
   switch (action.type) {
     case 'UPDATE_NAME':
@@ -21,14 +43,37 @@ function reducer(originalState, action) {
     default:
       return originalState;
   }
-}
+};
 
-//  dispatching the action
-let state2 = reducer(state, {
+//  using the store
+const storeInstance = new Store(
+  { firstName: 'Vivek', city: 'Haveri' },
+  reducer
+);
+
+//  registering the subscribers
+storeInstance.subscribe(() => {
+  console.log('Subscriber 1: State changed', storeInstance.getState());
+});
+
+storeInstance.subscribe(() => {
+  console.log('Subscriber 2: State changed', storeInstance.getState());
+});
+
+console.log('Dispatching the UPDATE_NAME action');
+
+//  dispatch the actions
+storeInstance.dispatch({
+  type: 'UPDATE_NAME',
+  payload: {
+    newFirstName: 'Rajendra',
+  },
+});
+
+console.log('Dispatching the UPDATE_CITY action');
+storeInstance.dispatch({
   type: 'UPDATE_CITY',
   payload: {
     newCity: 'Mumbai',
   },
 });
-
-console.log(state2);
